41414417f425809af1bef5dbbbc262d12fa679ed
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-14 20:11:45 +0300

    on-line work. 23-11-14

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        const int MINUTES_IN_HOUR = 60;
+        string name = "Vika";
+        Console.WriteLine($"Hello {name} \t How are you? \n Great!");
+
+        var flag = false;
+        var i = 10.0;
+        var fl = 10.1;
+
+        int j = Convert.ToInt32(i);
+        */
+
+        /*
+        //Решение задачи: купить продукты, купить мороженое, если хватит остатка
+        //Подготовка данных
+
+        //Свойства хлеба
+        bool isWhiteBradFresh = true;
+        ushort wHiteBredPrice = 150;
+
+        //Свойства масла
+        ushort butterPrice = 230;
+
+        //Свойства молока
+        ushort milkPrice = 170;
+        float milkFatPercentage = 1.2f;
+        */
+
+        //конвертация во float
+        int k = 10;
+        float j = Convert.ToSingle(k);
+        Console.WriteLine(j);
+
+    }
+}
