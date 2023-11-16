a452f5dcde2ca87f3e520016ac6c867d7f01bb9b
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-16 19:27:53 +0300

    Работа 16.11.23

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Runtime.Serialization;
+using System.Xml.Linq;
 
 internal class Program
 {
@@ -16,13 +17,13 @@ internal class Program
         int j = Convert.ToInt32(i);
         */
 
-        /*
+
         //Решение задачи: купить продукты, купить мороженое, если хватит остатка
         //Подготовка данных
 
         //Свойства хлеба
         bool isWhiteBradFresh = true;
-        ushort wHiteBredPrice = 150;
+        ushort whiteBredPrice = 150;
 
         //Свойства масла
         ushort butterPrice = 230;
@@ -30,12 +31,54 @@ internal class Program
         //Свойства молока
         ushort milkPrice = 170;
         float milkFatPercentage = 1.2f;
-        */
 
-        //конвертация во float
-        int k = 10;
-        float j = Convert.ToSingle(k);
-        Console.WriteLine(j);
+        //
+        ushort icecreamPrice = 170;
+
+        Console.Write("Введите начальную сумму: ");
+        string? sumString = Console.ReadLine();  // может быть введено mull
+        short sum = Convert.ToInt16(sumString);
+
+        if (sum > 0)
+        {
+            if (!isWhiteBradFresh)
+            {
+                Console.WriteLine("Хлеб не свежий(");
+            }
+            else if
+            {
+                if (sum >= whiteBredPrice)
+                { 
+                    sum = Convert.ToInt16(sum - whiteBredPrice); 
+                }
+                else 
+                {
+                    Console.WriteLine("На хлеб денег не хватает");
+                }
+               
+            }
+
+            sum = (short)(sum - butterPrice);
+
+            if (milkFatPercentage == 1.2f)
+            {
+                sum = Convert.ToInt16(sum - milkPrice);
+            }
+
+            if (sum >= icecreamPrice)
+            {
+                Console.WriteLine("Ура! Можно купить мороженое!"); 
+                sum = Convert.ToInt16(sum - icecreamPrice);
+            }
+            Console.WriteLine($"Остаток суммы: {sum}");
+        }
+
+        else
+        {
+            Console.WriteLine($"Сумма должна быть положительной ");
+        }
+
+ 
 
     }
 }
