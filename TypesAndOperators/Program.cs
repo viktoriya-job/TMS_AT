fc7c3f08e34134129c72e0f2e0e81346bc2f606c
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-19 15:48:20 +0300

    Работа на лекуии 231118 - Массивы и циклы

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //int[] array1;
+        //int n = 4;
+        //array1 = new int[n];
+
+        //int[] array = { 1, 9, 3, 6, 5, 8};
+
+        //Console.WriteLine(array[2]);
+        //Console.WriteLine(array[array.GetUpperBound(0)]);
+
+        //Console.WriteLine("Length: " + array.Length);                       // количество всех элементов
+        //Console.WriteLine("Rank: " + array.Rank);                           // ранк массива
+        //Console.WriteLine("GetLength(0): " + array.GetLength(0));           // количество строк
+        //Console.WriteLine("GetUpperBound(0): " + array.GetUpperBound(0));
+        //Console.WriteLine("GetUpperBound(0): " + array.GetLowerBound(0));   // Верхний индекс строк
+
+        //for (int i = 0; i <= array.GetUpperBound(0); i++) {
+        //    Console.WriteLine($"Элемент: {array[i]}");
+        //}
+
+        //for (int j = 0; j < array.GetLength(0); j++)
+        //{
+        //    Console.Write($"{array[j]} ");
+        //}
+        //foreach (int k in array) {
+        //        Console.Write($"{k} ");
+        //    }
+        
+
+    }
+}
