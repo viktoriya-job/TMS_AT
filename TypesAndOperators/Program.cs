a7cac3583fc4b2bec306545b2d5b8e9e31f9b3d6
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-19 18:29:03 +0300

    Task10 added - method SumOfAllMatrixElements

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -44,9 +44,38 @@ namespace TypesAndOperators
                     case 7: Task7.SortedArrayOfNames(); break;
                     case 8: Task8.BubbleSort(); break;
                     case 9: Task9.MultiplyTwoMatrices(); break;
+                    case 10: Task10.SumOfAllMatrixElements(); break;
                 }
             }
 
+            //Lection8_Arrays
+
+            //int[] array1;
+            //int n = 4;
+            //array1 = new int[n];
+
+            //int[] array = { 1, 9, 3, 6, 5, 8};
+
+            //Console.WriteLine(array[2]);
+            //Console.WriteLine(array[array.GetUpperBound(0)]);
+
+            //Console.WriteLine("Length: " + array.Length);                       // количество всех элементов
+            //Console.WriteLine("Rank: " + array.Rank);                           // ранк массива
+            //Console.WriteLine("GetLength(0): " + array.GetLength(0));           // количество строк
+            //Console.WriteLine("GetUpperBound(0): " + array.GetUpperBound(0));
+            //Console.WriteLine("GetUpperBound(0): " + array.GetLowerBound(0));   // Верхний индекс строк
+
+            //for (int i = 0; i <= array.GetUpperBound(0); i++) {
+            //    Console.WriteLine($"Элемент: {array[i]}");
+            //}
+
+            //for (int j = 0; j < array.GetLength(0); j++)
+            //{
+            //    Console.Write($"{array[j]} ");
+            //}
+            //foreach (int k in array) {
+            //        Console.Write($"{k} ");
+            //    }
         }
     }
 }
---
+++ TypesAndOperators/Task10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndOperators
+{
+    static class Task10
+    {
+        public static void SumOfAllMatrixElements()
+        {
+            Random random = new Random();
+
+            int[,] matrix = new int[4, 5];
+
+            //заполним матрицу случайными числами
+            for (byte i = 0; i < matrix.GetLength(0); i++)
+                for (byte j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = random.Next(100);
+
+            int summ = 0;
+            foreach (int element in matrix)
+                summ += element;
+
+            //Выведем получившуюся матрицу и итоговую сумму всех элементов матрицы
+            Console.WriteLine("Матрица: ");
+            for (byte resI = 0; resI < matrix.GetLength(0); resI++)
+            {
+                for (byte resJ = 0; resJ < matrix.GetLength(1); resJ++)
+                    Console.Write($"{matrix[resI, resJ]} "); ;
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\nСумма всех элементов матрицы: {summ}");
+        }
+    }
+}
