a441af7408f12915ffac4aa8ccde9888544bf88c
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-19 15:59:57 +0300

    Task1 added - method IsNumberIncludedInArray

---
+++ TypesAndOperators/Task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndOperators
+{
+    static class Task1
+    {
+        public static void IsNumberIncludedInArray()
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Введите число, вхождение которого в следующем массиве нужно проверить: \n");
+
+            int[] array = new int[10];
+            for (byte i = 0; i < array.GetLength(0); i++)
+            {
+                array[i] = random.Next(100);
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (Array.Exists(array, element => element == num))
+                Console.WriteLine($"Число {num} входит в массив");
+            else
+                Console.WriteLine($"Число {num} не входит в массив");
+        }
+    }
+}
+
