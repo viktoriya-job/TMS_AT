9e39a01f366e92e8192412a720e242f777caa259
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-12-02 21:13:34 +0300

    OOP

---
+++ OOP/Polymorphism/Program4.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program4
+{
+    private static void Main4(string[] args)
+    {
+        Individual individual1 = new Individual(1);
+        individual1.SendNotification();
+        individual1.Address = "Individual Address";
+
+
+        Corporate corporate1 = new Corporate(1);
+        corporate1.SendNotification();
+        corporate1.Address = "Office Address";
+
+
+        Console.WriteLine(individual1.Address);
+        Console.WriteLine(corporate1.Address);
+    }
+}
