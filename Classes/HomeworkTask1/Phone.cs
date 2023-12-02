47ab23ff64076dc89ae161fb7ffe73ee614f3eda
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-12-02 17:15:28 +0300

    Task1 added -class Phone

---
+++ Classes/HomeworkTask1/Phone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Homework
+{
+    internal class Phone
+    {
+        //Из соглашения https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/coding-style/identifier-names
+        //"При именовании public элементов типов, таких как поля, свойства, события, используйте регистр pascal"
+        //поэтому для public полей поля имена задаю в PascalCase
+
+        public string Number = "+7 (000) 000-00-00";
+        public string Model = "X-Ray";
+        public float Weight;
+
+        public Phone() { }
+
+        public Phone(string number, string model)
+        {
+            Number = number;
+            Model = model;
+        }
+
+        public Phone(string number, string model, float weight) : this(number, model)
+        {
+            Weight = weight;
+        }
+
+
+        public void ReceiveCall(string callerName)
+        {
+            Console.WriteLine($"Вам звонит {callerName}");
+        }
+
+        public string GetNumber() => Number;
+
+        public void SendMessage(params string[] numbersArray)
+        {
+            foreach (string number in numbersArray)
+                Console.WriteLine(number);
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"""
+
+                Номер телефона: {Number}
+                Модель: {Model}
+                Вес: {Weight}
+
+                """);
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,8 +10,46 @@ namespace Classes
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.OutputEncoding = Encoding.GetEncoding("utf-8");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //Задача 1
+            Console.WriteLine($"\n{new string('=', 15)} Задача 1 {new string('=', 15)}\n");
+
+            Phone phone1 = new Phone();
+            Phone phone2 = new Phone("890111110011", "Redmi");
+            Phone phone3 = new Phone("+7(901)11110011", "Redmi", 300);
+
+            phone1.PrintInfo();
+            phone2.PrintInfo();
+            phone3.PrintInfo();
+
+            phone1.ReceiveCall("Реклама");
+            phone2.ReceiveCall("Мария Александровна КлласРук о ужас");
+            phone3.ReceiveCall("Служба поддержки Сбербанка");
+
+            Console.WriteLine();
+            Console.WriteLine(phone1.GetNumber());
+            Console.WriteLine(phone2.GetNumber());
+            Console.WriteLine(phone3.GetNumber());
+
+            Console.WriteLine();
+            phone1.SendMessage(
+                "8 (901) 111-11-11",
+                "8 (901) 111-11-12",
+                "8 (901) 111-11-13",
+                "8 (901) 111-11-14",
+                "8 (901) 111-11-15");
+
+            Console.WriteLine();
+            phone2.SendMessage(
+                "8 (901) 111-11-21",
+                "8 (901) 111-11-22");
+
+            Console.WriteLine($"\n{new string('=', 15)} Задача 1 {new string('=', 15)}\n");
+
+
 
             TreatmentPlan treatmentPlan1 = new TreatmentPlan(1);
             TreatmentPlan treatmentPlan2 = new TreatmentPlan(2);
