7e1a1175c44e9673a30d61de9d7bb40be657eeea
Вика Александрова <viktoriya_job@mail.ru>
2023-12-10 00:04:09 +0300

    Library added

---
+++ ClassLibrary1/FinancialInvoice.cs
@@ -0,0 +1,27 @@
+﻿namespace DocumentsAccount
+{
+    public class FinancialInvoice : Document
+    {
+        public float TotalAmount { get; set; } = 0;
+        public string DepartmentCode { get; set; } = "Не определен";
+
+        public FinancialInvoice() { }
+
+        public FinancialInvoice(string number, DateOnly date, float totalAmount, string departmentCode) : base(number, date)
+        {
+            TotalAmount = totalAmount;
+            DepartmentCode = departmentCode;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"""
+                Итоговая сумма за месяц:    {TotalAmount}
+                Дата документа:             {Date}
+                Номер документа:            {Number}
+                Код департамента:           {DepartmentCode}
+                {new string('~', 35)}
+                """);
+        }
+    }
+}
