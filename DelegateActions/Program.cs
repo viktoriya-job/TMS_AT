eac092d060794402c9097cbf60b8f5f473d0d982
Вика Александрова <viktoriya_job@mail.ru>
2024-01-13 10:22:32 +0300

    Task1 added

--- DelegateActions/Program.cs
+++ DelegateActions/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿using System.Text;
+using Delegate;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+
+        Console.WriteLine("""
+                Выберите задачу:
+                1 -  Задача 1: Делегат без входных параметров
+                2 -  Задача 2: 
+                3 -  Задача 3: 
+                4 -  Задача 4:
+                """);
+
+        if (CheckInputHelper.IsCorrectInt32(Console.ReadLine(), out int task))
+        {
+            Console.Clear();
+            switch (task)
+            {
+                case 1:
+                    Task1 task1 = new Task1();
+                    task1.Run();
+                    break;
+
+
+                default: Console.WriteLine("Вы ввели неправильный номер"); break;
+            }
+        }
     }
 }
---
+++ DelegateActions/Task1/Task1.cs
@@ -0,0 +1,19 @@
+﻿namespace Delegate
+{
+    internal class Task1
+    {
+        public delegate int Random10Delegate();
+
+        public void Run()
+        {
+            Random10Delegate random10Delegate = Random10;
+            Console.WriteLine(random10Delegate());
+        }
+
+        public int Random10()
+        {
+            Random random = new Random();
+            return random.Next(10);
+        }
+    }
+}
