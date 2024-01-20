5a6781eeae8ab18bfc5368be5375cd24f3701aa6
Вика Александрова <viktoriya_job@mail.ru>
2024-01-20 14:36:22 +0300

    commit

---
+++ NUnitTest/SimpleTest.cs
@@ -0,0 +1,12 @@
+namespace NUnitTest
+{
+    public class SimpleTest
+    {
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine($"{this}: Test...");
+            Assert.Pass();
+        }
+    }
+}
