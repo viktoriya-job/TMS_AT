6bbc1fb147c82eea4458b6a747bea2225330511d
Вика Александрова <viktoriya_job@mail.ru>
2024-01-26 20:39:03 +0300

    Calc modified

---
+++ NUnitTest/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace NUnitTest;
+
+public static class Calc
+{
+    public static int Sum(int x, int y) => x + y;
+
+    public static int Div(int x, int y) => x / y;
+    
+    public static double Div(double x, double y) => x / y;
+}
