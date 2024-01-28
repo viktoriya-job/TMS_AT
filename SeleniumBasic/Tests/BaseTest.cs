765cdbdac99ef12a9b2e41c0a0887f105603a86e
Вика Александрова <viktoriya_job@mail.ru>
2024-01-28 23:17:37 +0300

    Work 27/01

--- SeleniumBasic/Tests/BaseTest.cs
+++ SeleniumBasic/Tests/BaseTest.cs
@@ -1,22 +1,26 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumBasic.Core;
 
 namespace SeleniumBasic.Tests
 {
-    internal class BaseTest
+    [Parallelizable(scope: ParallelScope.All)]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public class BaseTest
     {
-        //protected IWebDriver Driver { get; set; }
+        protected IWebDriver Driver { get; set; }
 
-        //[SetUp]
-        //public void Setup()
-        //{ 
-        //    Driver = new Browser().Driver!;
-        //    Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
-        //}
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new Browser().Driver!;
+            //Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+        }
 
-        //[TearDown]
-        //public void TearDown()
-        //{
-        //    Driver.Quit();
-        //}
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
     }
 }
