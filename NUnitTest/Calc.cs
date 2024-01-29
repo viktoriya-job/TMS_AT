f44651e26ba87045e585309f3645a77256be6278
Вика Александрова <viktoriya_job@mail.ru>
2024-01-29 13:59:50 +0300

    fix1

--- NUnitTest/Calc.cs
+++ NUnitTest/Calc.cs
@@ -10,8 +10,8 @@ public class Calc
     
     public double Div(double a, double b)
     {
-        if (b == 0)
-            throw new DivideByZeroException(); //без if для double возвращается null в случае деления на 0 - прокинула Exception
+        if (double.IsInfinity(a/b))
+            throw new DivideByZeroException();
         return a / b;
     }
 }
