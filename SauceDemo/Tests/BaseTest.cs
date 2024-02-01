baf4a4cd0d6bc7c5a33de9ccf08be06bed49e444
Вика Александрова <viktoriya_job@mail.ru>
2024-02-01 13:56:36 +0300

    Init

---
+++ SauceDemo/Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SauceDemo.Core;
+
+namespace SauceDemo.Tests
+{
+    [Parallelizable(scope: ParallelScope.All)]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public class BaseTest
+    {
+        protected IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new Browser().Driver!;
+            //Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ SauceDemo/Tests/SauceDemoTests.cs
@@ -0,0 +1,21 @@
+﻿
+using OpenQA.Selenium;
+
+namespace SauceDemo.Tests
+{
+    internal class SauceDemoTest : BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Navigate().GoToUrl("https://www.saucedemo.com/"); ;
+        }
+
+        [Test]
+        public void SauceDemoMultiTest()
+        {
+            //Driver.Navigate().GoToUrl("https://www.saucedemo.com/"); ;
+            Assert.That(Driver.FindElement(By.Id("root")).Displayed);
+        }
+    }
+}
