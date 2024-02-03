57b1bab0d47a5f4d24e1697360e255f5b4c2c14b
Вика Александрова <viktoriya_job@mail.ru>
2024-02-03 11:44:21 +0300

    fix

--- LINQ/Task2/Task2.cs
+++ LINQ/Task2/Task2.cs
@@ -13,18 +13,26 @@ namespace LINQ
                 new Client(1,2024,1,24),
                 new Client(1,2024,2,8),
                 new Client(2,2024,1,18),
-                new Client(2,2024,2,18),
                 new Client(3,2024,1,8),
+                new Client(2,2024,2,18),
+                new Client(3,2024,3,8),
                 new Client(3,2024,2,10),
                 new Client(4,2024,1,14),
                 new Client(4,2024,2,16),
             };
 
-            var minTrainingDuration = _clientsList.Where(client => client.TrainingDuration == _clientsList.Min(client => client.TrainingDuration)).Last();
+            //var min = _clientsList.Min(client => client.TrainingDuration);
+            //var minTrainingDuration = _clientsList.Where(client => client.TrainingDuration == min).Last();
             
-            Console.WriteLine($"TrainingDuration: {minTrainingDuration.TrainingDuration}" +
-                $", Year: {minTrainingDuration.Year}" +
-                $", Month: {minTrainingDuration.Month}");
+            //Console.WriteLine($"TrainingDuration: {minTrainingDuration.TrainingDuration}" +
+            //    $", Year: {minTrainingDuration.Year}" +
+            //    $", Month: {minTrainingDuration.Month}");
+
+            var minTrainingDuration1 = _clientsList.OrderByDescending(c => c.TrainingDuration).Last();
+
+            Console.WriteLine($"TrainingDuration: {minTrainingDuration1.TrainingDuration}" +
+                $", Year: {minTrainingDuration1.Year}" +
+                $", Month: {minTrainingDuration1.Month}");
         }
     }
 }
