ce494ef8b45e9d8f2a634045297768e34ed59150
Вика Александрова <viktoriya_job@mail.ru>
2024-02-08 10:25:51 +0300

    FileUploadSuccessTest added

---
+++ SeleniumBasic/Tests/FileUploadTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System.Reflection;
+
+namespace SeleniumAdvanced.Tests
+{
+    public class FileUploadTest : BaseTest
+    {
+        [Test]
+        public void FileUploadSuccessTest()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/upload");
+
+            string location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(location, "Resources", "msg1205352859-4623.jpg");
+            var fileUploadInput = WaitsHelper.WaitForExists(By.Id("file-upload"));
+            var fileUploadSubmitButton = WaitsHelper.WaitForExists(By.Id("file-submit"));
+
+            fileUploadInput.SendKeys(path);
+            fileUploadSubmitButton.Click();
+
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("uploaded-files")).Text, Is.EqualTo("msg1205352859-4623.jpg"));
+        }
+    }
+}
