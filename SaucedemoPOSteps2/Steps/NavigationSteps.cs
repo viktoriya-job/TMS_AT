2cc1a5f97dba735cb5605097ee7c3aedad9679bd
Вика Александрова <viktoriya_job@mail.ru>
2024-02-16 19:48:01 +0300

    -

--- SaucedemoPOSteps2/Steps/NavigationSteps.cs
+++ SaucedemoPOSteps2/Steps/NavigationSteps.cs
@@ -8,30 +8,7 @@ namespace SaucedemoPOSteps.Steps
     {
         public NavigationSteps(IWebDriver driver) : base(driver) { }
 
-        public InventoryPage SuccessLogin(string username, string password)
-        {
-            LoginPage.UsernameInput().SendKeys(username);
-            LoginPage.PasswordInput().SendKeys(password);
-            LoginPage.LoginButton().Click();
-
-            return new InventoryPage(Driver);
-        }
-
-        public LoginPage FailureLogin(string username, string password)
-        {
-            LoginPage.UsernameInput().SendKeys(username);
-            LoginPage.PasswordInput().SendKeys(password);
-            LoginPage.LoginButton().Click();
-
-            return LoginPage;
-        }
-
         public LoginPage NavigateToLoginPage() => LoginPage;
-        public CartPage NavigateToCartPage()
-        {
-            //NavigateToLoginPage();
-            //SuccessLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password);
-            return new CartPage(Driver, true);
-        }
+        public CartPage NavigateToCartPage() => new CartPage(Driver, true);
     }
 }
