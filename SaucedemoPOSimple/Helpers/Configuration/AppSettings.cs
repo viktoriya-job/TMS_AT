9cc00bd8f6352ea135c24c95fe662ccd35452f37
Вика Александрова <150160365+viktoriya-job@users.noreply.github.com>
2024-02-17 23:02:17 +0300

    Delete SaucedemoPOSimple directory

--- SaucedemoPOSimple/Helpers/Configuration/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace SaucedemoPOSimple.Helpers.Configuration
-{
-    public class AppSettings
-    {
-        public string? URL { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
