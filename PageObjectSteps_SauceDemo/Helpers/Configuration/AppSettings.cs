c733e397bbd8ada1735f3e8e926929a827027bb4
Вика Александрова <150160365+viktoriya-job@users.noreply.github.com>
2024-02-18 20:05:22 +0300

    Delete PageObjectSteps_SauceDemo directory

--- PageObjectSteps_SauceDemo/Helpers/Configuration/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Wrappers1.Helpers.Configuration
-{
-    public class AppSettings
-    {
-        public string? URL { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
