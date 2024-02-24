daec3f50d80a905254b9357b49e3df0adf9d8008
Вика Александрова <viktoriya_job@mail.ru>
2024-02-24 12:01:24 +0300

    init

--- Wrappers/Elements/Checkbox.cs
+++ Wrappers/Elements/Checkbox.cs
@@ -1,7 +1,22 @@
-﻿namespace Wrappers.Elements;
+﻿using OpenQA.Selenium;
+
+namespace Wrappers.Elements;
 
 public class Checkbox
 {
+    private UIElement _uiElement;
+
+    public Checkbox(IWebDriver webDriver, By by)
+    {
+        _uiElement = new UIElement(webDriver, by);
+    }
+    public Checkbox(IWebDriver webDriver, IWebElement webElement)
+    {
+        _uiElement = new UIElement(webDriver, webElement);
+    }
+
+    public bool Displayed => _uiElement.Displayed;
+
     public void Click()
     {
 
