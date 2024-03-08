b9c060936be6b44c19be9e538e3b6c8f9eb0ae78
Вика Александрова <viktoriya_job@mail.ru>
2024-03-08 23:13:26 +0300

    Milestone added

---
+++ Task3/Elements/Calendar.cs
@@ -0,0 +1,25 @@
+﻿
+using OpenQA.Selenium;
+
+namespace Task3.Elements
+{
+    public class Calendar
+    {
+        private UIElement _uiElement;
+
+        public Calendar(IWebDriver webDriver, By locator)
+        {
+            _uiElement = new UIElement(webDriver, locator);
+        }
+
+        public bool Displayed => _uiElement.Displayed;
+
+        public void Click() => _uiElement.Click();
+
+        private void SelectValue(UIElement value)
+        {
+            _uiElement.Click();
+            _uiElement.FindUIElement(By.XPath($"//a[text()='{value}']")).Click();
+        }
+    }
+}
--- Task3/Models/Milestone.cs
+++ Task3/Models/Milestone.cs
@@ -1,6 +1,106 @@
-﻿namespace Task3.Models
+﻿using Task3.Elements;
+
+namespace Task3.Models
 {
-    internal class Milestone
+    public class Milestone
     {
+        public string Name { get; }
+        public string References { get; }
+        public int Parent { get; }
+        public string Description { get; }
+        public int StartDate { get; }
+        public int EndDate { get; }
+        public bool IsMilestoneCompleted { get; }
+
+        private Milestone(string name, string references, int parent, string description, int startDate, int endDate, bool isMilestoneCompleted)
+        {
+            Name = name;
+            References = references;
+            Parent = parent;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+            IsMilestoneCompleted = isMilestoneCompleted;
+        }
+
+        public class Builder
+        {
+            public string Name { get; set; }
+            public string References { get; set; }
+            public int Parent { get; set; }
+            public string Description { get; set; }
+            public int StartDate { get; set; }
+            public int EndDate { get; set; }
+            public bool IsMilestoneCompleted { get; set; }
+
+            private Builder(string name, string references, int parent, string description, int startDate, int endDate, bool isMilestoneCompleted)
+            {
+                Name = name;
+                References = references;
+                Parent = parent;
+                Description = description;
+                StartDate = startDate;
+                EndDate = endDate;
+                IsMilestoneCompleted = isMilestoneCompleted;
+            }
+
+            public Builder SetName(string name)
+            {
+                Name = name;
+                return this;
+            }
+
+            public Builder SetReferences(string references)
+            {
+                References = references;
+                return this;
+            }
+
+            public Builder SetParent(int parent)
+            {
+                Parent = parent;
+                return this;
+            }
+
+            public Builder SetDescription(string description)
+            {
+                Description = description;
+                return this;
+            }
+
+            public Builder SetStartDate(int startDate)
+            {
+                StartDate = startDate;
+                return this;
+            }
+
+            public Builder SetEndDate(int endDate)
+            {
+                EndDate = endDate;
+                return this;
+            }
+
+            public Builder SetEndDate(bool isMilestoneCompleted)
+            {
+                IsMilestoneCompleted = isMilestoneCompleted;
+                return this;
+            }
+
+            public Milestone Build()
+            {
+                if (string.IsNullOrWhiteSpace(Name))
+                    throw new InvalidOperationException("Name can not be null");
+
+                return new Milestone(
+                    Name,
+                    References,
+                    Parent,
+                    Description,
+                    StartDate,
+                    EndDate,
+                    IsMilestoneCompleted
+                    );
+            }
+        }
     }
 }
