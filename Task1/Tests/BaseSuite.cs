39f8406540e65ba94d10aba6f41a1727205772a0
Вика Александрова <viktoriya_job@mail.ru>
2024-03-13 19:59:00 +0300

    Class work

---
+++ Task1/Tests/BaseSuite.cs
@@ -0,0 +1,17 @@
+﻿using NLogExample.Core;
+
+namespace Task1.Tests
+{
+    [SetUpFixture]
+    public class BaseSuite
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        [OneTimeSetUp]
+        public static void SuiteSetup()
+        {
+            new NLogConfig().Config();
+            Logger.Info("NLOG Setted Up");
+        }
+    }
+}
---
+++ Task1/Tests/NLogTest.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class NLogTest
+    {
+        protected static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        [Test]
+        public void LevelsTest()
+        {
+            Logger.Log(LogLevel.Info, "Some info");
+
+            Logger.Trace("message Trace");
+            Logger.Debug("message Debug");
+            Logger.Info("message Info");
+            Logger.Warn("message Warn");
+            Logger.Error("message Error");
+            Logger.Fatal("message Fatal");
+        }
+
+        [Test]
+        public void ExceptionLogTest()
+        {
+            try
+            {
+                throw new AssertionException("Какая-то ошибка..");
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "Логируем ошибку....");
+            }
+        }
+    }
+}
