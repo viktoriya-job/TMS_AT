3940fc481a95d1ea6f2a699cd0c67d7bb9453c81
Вика Александрова <viktoriya_job@mail.ru>
2024-03-14 21:12:42 +0300

    Work 14/03

---
+++ ValueOfObjects/Models/JsonObject1.cs
@@ -0,0 +1,15 @@
+﻿namespace ValueOfObjects.Models
+{
+    public class JsonObject1
+    {
+        public string Name { get; set; }
+        public int FieldValue {  get; set; }
+        public bool IsEnabled { get; set; }
+        public object AnyObject { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(FieldValue)}: {FieldValue}, {nameof(IsEnabled)}: {IsEnabled}, {nameof(AnyObject)}: {AnyObject}";
+        }
+    }
+}
