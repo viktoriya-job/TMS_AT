0cf3ec03a1a91c22f8198dcaf49cf7ce48597630
Вика Александрова <viktoriya_job@mail.ru>
2024-03-14 22:35:58 +0300

    -

--- SpecFlowTestProject2/StepDefinitions/FirstStepDefinition.cs
+++ SpecFlowTestProject2/StepDefinitions/FirstStepDefinition.cs
@@ -1,80 +1,70 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecFlowTestProject.Driver;
 
-namespace SpecFlowTestProject.StepDefinitions
+namespace SpecFlowTestProject;
+
+[Binding]
+public class FirstStepDefinition
 {
-    [Binding]
-    public class FirstStepDefinition
+    // private readonly Browser _browser;
+
+    //public FirstStepDefinition(Browser browser)
+    //{
+    //    _browser = browser;
+    //}
+
+    private IWebDriver? _driver;
+
+    [When("страница логина открыта")]
+    [Given("страница логина открыта")]
+    public void OpenLoginPage()
     {
-        //private readonly Browser _browser;
+        Console.WriteLine("Login page is opened...");
+        //_browser.Driver.Navigate().GoToUrl("https://aqa2503.testrail.io/");
+    }
 
-        //public FirstStepDefinition(Browser browser)
-        //{
-        //    _browser = browser;
-        //}
+    [Then(@"username field is displayed")]
+    public void IsUsernameFieldDisplayed()
+    {
+        Assert.That(true);
+    }
 
-        [Given(@"открыт браузер")]
-        public void OpenBrowser()
-        {
-            Console.WriteLine("Browser is opened...");
-        }
+    [Then(@"login button is disabled")]
+    public void IsLoginButtonDisabled()
+    {
+        Assert.That(true);
+    }
 
+    [When(@"user ""(.*)"" with password ""(.*)"" logged in")]
+    public void UserWithCredentialsLoggedIn(string username, string password)
+    {
+        Console.WriteLine($"User {username} logged in with psw {password}");
+    }
 
-        [Given(@"открыт новый браузер")]
-        public void GivenОткрытНовыйБраузер()
-        {
-            Console.WriteLine("New browser is opened...");
-        }
+    [Then(@"the add project button is displayed")]
+    public void ThenTheAddProjectButtonIsDisplayed()
+    {
+        Console.WriteLine("Button is displayed...");
+    }
 
+    [Then(@"username is ""(.*)""")]
+    public void IsUsernameEquals(string username)
+    {
+        Console.WriteLine($"User has {username}");
+    }
 
-        [When("страница логина открыта")]
-        [Given("страница логина открыта")]
-        public void OpenLoginPage()
-        {
-            Console.WriteLine("Login page is opened...");
-            //_browser.Driver.Navigate().GoToUrl("https://aqa2503.testrail.io/");
-        }
+    [Then(@"a project ID is (.*)")]
+    public void IsProjectIdEqualsTo(int id)
+    {
+        Assert.That(id, Is.EqualTo(23));
+    }
 
-        [Then(@"username field is displayed")]
-        public void IsUsernameFieldDisplayed()
-        {
-            Assert.That(true);
-        }
-
-        [Then(@"login button is disabled")]
-        public void IsLoginButtonDisabled()
-        {
-            Assert.That(true);
-        }
-
-        //[When(@"user ""(.*)"" with password ""(.*)"" logged in")]
-        //public void UserWithCredentialsLoggedIn(string username, string password)
-        //{
-        //    Console.WriteLine($"User {username} logged in with psw {password}");
-        //}
-
-        //[Then(@"the add project button is displayed")]
-        //public void ThenTheAddProjectButtonIsDisplayed()
-        //{
-        //    Console.WriteLine("Button is displayed...");
-        //}
-
-        //[Then(@"username is ""(.*)""")]
-        //public void IsUsernameEquals(string username)
-        //{
-        //    Console.WriteLine($"User has {username}");
-        //}
-
-        //[Then(@"a project ID is (.*)")]
-        //public void IsProjectIdEqualsTo(int id)
-        //{
-        //    Assert.That(id, Is.EqualTo(23));
-        //}
-
-        //[Given(@"a blog post named ""(.*)"" with Markdown body")]
-        //public void GivenABlogPostNamedWithMarkdownBody(string random, string multilineText)
-        //{
-        //    Console.WriteLine($"Blog has name : {random}");
-        //    Console.WriteLine($"Text : {multilineText}");
-        //}
+    [Given(@"a blog post named ""(.*)"" with Markdown body")]
+    public void GivenABlogPostNamedWithMarkdownBody(string random, string multilineText)
+    {
+        Console.WriteLine($"Blog has name : {random}");
+        Console.WriteLine($"Text : {multilineText}");
     }
 }
